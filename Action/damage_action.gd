b04368f1684af3397c113a370f1cedac9bf057be
Joe Colley <josephthco@gmail.com>
2025-10-29 13:20:12 -0400

    remove and reset

--- Action/damage_action.gd
+++
@@ -1,9 +0,0 @@
-extends Action
-class_name DamageAction
-
-
-@export var damage_amount: int = 1
-
-
-func execute(being: Being) -> void:
-	being.health.take_damage(self)
--- Action/shield_action.gd
+++
@@ -1,9 +0,0 @@
-extends Action
-class_name ShieldAction
-
-
-@export var shield_amount: int = 1
-
-
-func execute(being: Being) -> void:
-	being.health.give_shield(self)
