883b7d4624f60f786f9a0bb8cc0caa00e193442e
Joe Colley <josephthco@gmail.com>
2025-11-07 15:06:50 -0500

    dc_6

--- Menus/MainMenu/main_menu.gd
+++ Menus/MainMenu/main_menu.gd
@@ -2,5 +2,12 @@ extends Control
 class_name MainMenu
 
 
+@export var version_label: Label
+const PRE_VERSION: String = "dc_"
+
+func _ready() -> void:
+	version_label.text = PRE_VERSION + ProjectSettings.get_setting("application/config/version")
+
+
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://Game/game.tscn")
